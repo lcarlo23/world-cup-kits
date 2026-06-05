33867d9557932a983783ddba7262f36bb53e5f0b
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-06-05 11:47:14 +0200

    Add swagger configuration files

---
+++ swagger.js
@@ -0,0 +1,33 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'World Cup Kits API',
+    description: 'API documentation for the World Cup Kits e-commerce shop (Final Project)',
+    version: '1.0.0',
+  },
+  host: '',
+  schemes: ['http', 'https'],
+  securityDefinitions: {
+    github_auth: {
+      type: 'oauth2',
+      authorizationUrl: '/login',
+      flow: 'implicit',
+      scopes: {},
+    },
+  },
+  tags: [
+    { name: 'Products', description: 'Operations related to products' },
+    { name: 'Users', description: 'Operations related to users' },
+    { name: 'Orders', description: 'Operations related to orders' },
+    { name: 'Reviews', description: 'Operations related to reviews' }
+  ]
+};
+
+const outputFile = './swagger.json';
+const routes = ['./routes/index.js']; 
+
+// Genera il file swagger.json
+swaggerAutogen()(outputFile, routes, doc).then(() => {
+  console.log('Swagger documentation generated successfully!');
+});
